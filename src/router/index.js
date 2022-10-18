69192cf77ee8f7bd4bb21f48bef2f7065ac9324f
Burak <48869708+drementer@users.noreply.github.com>
2022-10-18 10:48:56 +0300

    Fast pouasdasdas

--- src/router/index.js
+++ src/router/index.js
@@ -10,7 +10,7 @@ Vue.use(VueRouter);
 const routes = [...Home, ...About, ...GetStarted];
 
 const router = new VueRouter({
-  mode: 'history',
+  /*   mode: 'history', */
   base: process.env.BASE_URL,
   routes,
 });
