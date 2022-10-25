1cee63e049c936ea496366117d2baabc8ae6a9f4
Burak <48869708+drementer@users.noreply.github.com>
2022-10-25 12:23:08 +0300

    Added new componenet

---
+++ src/components/CustomInput.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="form-element -input">
+    <label for="">Custom input</label>
+    <input type="text" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CustomInput',
+};
+</script>
